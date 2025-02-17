6f804286ed1ce14cefe3793c62cd90ac4b8368d1
rahul-thakur-01 <rahulthakur.developer@gmail.com>
2025-02-17 17:22:45 +0530

    configure api-server

---
+++ api-server/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const { generateSlug } = require('random-word-slugs')
+const { ECSClient, RunTaskCommand } = require('@aws-sdk/client-ecs')
+
+
+require('dotenv').config()
+
+const app = express()
+const PORT = 9000
+
+const ecsClient = new ECSClient({
+    region: 'ap-south-1',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+})
+
+const config = {
+    CLUSTER: process.env.AWS_ECS_CLUSTER_ARN,
+    TASK: process.env.AWS_ECS_TASK_ARN
+}
+
+app.use(express.json())
+
+app.post('/project', async (req, res) => {
+    const { gitURL, slug } = req.body
+    const projectSlug = slug ? slug : generateSlug()
+
+    // Spin the container
+    const command = new RunTaskCommand({
+        cluster: config.CLUSTER,
+        taskDefinition: config.TASK,
+        launchType: 'FARGATE',
+        count: 1,
+        networkConfiguration: {
+            awsvpcConfiguration: {
+                assignPublicIp: 'ENABLED',
+                subnets: process.env.AWS_SUBNETS.split(','),
+                securityGroups: process.env.AWS_SECURITY_GROUPS.split(',')
+                
+            }
+        },
+        overrides: {
+            containerOverrides: [
+                {
+                    name: 'builder-image',
+                    environment: [
+                        { name: 'GIT_REPOSITORY_URL', value: gitURL },
+                        { name: 'PROJECT_ID', value: projectSlug }
+                    ]
+                }
+            ]
+        }
+    })
+
+    await ecsClient.send(command);
+
+    return res.json({ status: 'queued', data: { projectSlug, url: `http://${projectSlug}.localhost:8000` } })
+
+})
+
+
+
+app.listen(PORT, () => console.log(`API Server Running..${PORT}`))
